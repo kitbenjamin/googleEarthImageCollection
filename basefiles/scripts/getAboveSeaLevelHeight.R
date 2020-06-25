7626d287bbbeaf5bee90133b7eef4acb2f60637a
willmorrison1 <willmorrison661@gmail.com>
2020-06-25 19:16:23 +0100

    wip GM heights to raster
    a working example of Gm (google maps) heights in raster format. Needs integrating into workflow

--- basefiles/scripts/getAboveSeaLevelHeight.R
+++ basefiles/scripts/getAboveSeaLevelHeight.R
@@ -1,6 +1,6 @@
 rawHeightAboveSeaLevelHeightFromGoogleAPI <- function(WGS84lat, WGS84lon, return.call = "json", 
-                                                         apiKey = "AIzaSyDiklf_BgSg67hfNzrJXEWeNcgrOXQzRGY",
-                                                         verbose = FALSE){
+                                                      apiKey = "AIzaSyDiklf_BgSg67hfNzrJXEWeNcgrOXQzRGY",
+                                                      verbose = FALSE){
   suppressWarnings(suppressMessages(require(RCurl)))
   suppressWarnings(suppressMessages(require(RJSONIO)))
   #get *single* height above sea level [has capability of multiple returns but CBA]
@@ -15,7 +15,7 @@ rawHeightAboveSeaLevelHeightFromGoogleAPI <- function(WGS84lat, WGS84lon, return
 
 formatHeightAboveSeaLevelHeightFromGoogleAPI <- function(heightJSON){
   #return *single* height above sea level [has capability of multiple returns but CBA]
-
+  
   return(c("lat" = heightJSON[[1]][[1]]$location$lat, "lon" = heightJSON[[1]][[1]]$location$lng, "elevation" = heightJSON[[1]][[1]]$elevation))
 }
 
@@ -28,32 +28,50 @@ getHeightAboveSeaLevelFromGoogleAPI <- function(WGS84lat, WGS84lon, return.call 
   return(formatHeightAboveSeaLevelHeightFromGoogleAPI(heightJSON))
 }
 
-multipleRawHeightAboveSeaLevelHeightFromGoogleAPI <- function(twoDlatlons, return.call = "json", 
-                                                      apiKey = "AIzaSyDiklf_BgSg67hfNzrJXEWeNcgrOXQzRGY",
-                                                      verbose = FALSE){
+
+GM_aslheight_api_call_chunk <- function(twoDlatlons, return.call = "json", 
+                                        apiKey = "AIzaSyDiklf_BgSg67hfNzrJXEWeNcgrOXQzRGY",
+                                        verbose = FALSE){
   suppressWarnings(suppressMessages(require(RCurl)))
   suppressWarnings(suppressMessages(require(RJSONIO)))
-  pasted = apply(twoDlatlons, 1, function(x){paste0(x[1], ',', x[2])})
-  toSubmit = paste0(pasted, collapse = '|')
+  pasted <- apply(twoDlatlons, 1, function(x){paste0(x[1], ',', x[2])})
+  toSubmit <- paste0(pasted, collapse = '|')
   #get *single* height above sea level [has capability of multiple returns but CBA]
   root <- "https://maps.googleapis.com/maps/api/elevation/"
-  address <- paste(root, return.call, "?locations=", toSubmit, "&key=",apiKey, sep = "")
-  if(verbose) print(address)
+  address <- paste(root, return.call, "?locations=", toSubmit, "&key=", apiKey, sep = "")
+  if (verbose) print(address)
   doc <- getURL(address)
-  x <- fromJSON(doc,simplify = FALSE)
+  x <- fromJSON(doc, simplify = FALSE)
   if (x$status != "OK") stop("apiKey error")
   return(x)
 }
 
-multipleFormatHeightAboveSeaLevelHeightFromGoogleAPI <- function(heightJSON){
-  formatted <- lapply(heightJSON[[1]], function(y){c("lat" = y$location$lat, "lon" = y$location$lng, "elevation" = y$elevation)})
-  return(formatted)
+GM_format_api_result <- function(GM_call_result) {
+  
+  lat_lon <-  do.call(rbind, lapply(GM_call_result$results, function(x) t(x$location)))
+  Z <-  sapply(GM_call_result$results, function(x) x$elevation)
+  return(data.frame(lat = as.numeric(lat_lon[,1]), 
+                    lon = as.numeric(lat_lon[,2]), 
+                    Z = Z))
 }
 
-getMultipleHeightAboveSeaLevelFromGoogleAPI <- function(twoDlatlons, return.call = "json", 
-                                                apiKey = "AIzaSyDiklf_BgSg67hfNzrJXEWeNcgrOXQzRGY",
-                                                verbose = FALSE){
+
+make_lat_lon_grid <- function(latStart, lonStart, grid_extent_padding, 
+                              grid_resolution) {
   
-  heightJSON <- multipleRawHeightAboveSeaLevelHeightFromGoogleAPI(twoDlatlons)
-  return(multipleFormatHeightAboveSeaLevelHeightFromGoogleAPI(heightJSON))
+  #lat is northing
+  northingSequence <- seq(params$regionArea$latStart - grid_extent_padding, 
+                          params$regionArea$latStart + params$regionArea$latSize + grid_extent_padding, 
+                          by = grid_resolution)
+  
+  #lon is eating
+  eastingSequence <- seq(params$regionArea$lonStart - grid_extent_padding, 
+                         params$regionArea$lonStart + params$regionArea$lonSize + grid_extent_padding,
+                         by = grid_resolution)
+  
+  pointGrid <- expand.grid(northing = northingSequence,
+                           easting = eastingSequence)
+  
+  return(pointGrid)
 }
+
