77209834e86aadf07e9f89fe57f3278dc18be5bc
willmorrison1 <willmorrison661@gmail.com>
2020-06-26 11:21:14 +0100

    finalised GM height raster

--- basefiles/scripts/getAboveSeaLevelHeight.R
+++ basefiles/scripts/getAboveSeaLevelHeight.R
@@ -77,3 +77,86 @@ make_lat_lon_grid <- function(latStart, lonStart, grid_extent_padding,
   return(pointGrid)
 }
 
+makeSPointsForGMheights <- function(latStart, 
+                                    lonStart, 
+                                    grid_extent_padding, 
+                                    grid_resolution, 
+                                    CRSstring, 
+                                    CRSstring_GM) {
+  
+  pointGrid_GE <- make_lat_lon_grid(latStart = latStart, 
+                                    lonStart = lonStart, 
+                                    grid_extent_padding = grid_extent_padding, 
+                                    grid_resolution = grid_resolution)
+  
+  pointGrid_sp <- SpatialPoints(pointGrid_GE[2:1], proj4string = CRS(CRSstring))
+  
+  pointGrid_sp_WGS84 <- spTransform(pointGrid_sp, CRS(CRSstring_GM))
+  
+  return(pointGrid_sp_WGS84)
+}
+
+GM_getAslRaster <- function(GMSPgrid, 
+                            callPause_sec = 2,
+                            CRSstring, 
+                            CRSstring_GM = "+init=epsg:4326", 
+                            maxCoordsPerCall = 400) {
+  
+  pointGrid_df <- data.frame(GMSPgrid)
+  seq_ind <- seq(1, nrow(pointGrid_df), by = maxCoordsPerCall)
+  #preallocate
+  oMat <- matrix(nrow = nrow(pointGrid_df), ncol = 3)
+  for (i in 1:length(seq_ind)) {
+    from <- seq_ind[i]
+    if (i == length(seq_ind)) {
+      to <- nrow(pointGrid_df)
+    } else {
+      to <- seq_ind[i + 1]
+    }
+    ind <- from:to
+    iter_vals <- GM_aslheight_api_call_chunk(pointGrid_df[ind,][2:1])
+    oMat[ind, ] <- as.matrix(GM_format_api_result(iter_vals))
+    if (i > 1) Sys.sleep(callPause_sec)
+  }
+  
+  pointGrid_df_z <- SpatialPointsDataFrame(data.frame(oMat[,2:1]),
+                                           data.frame("Z" = oMat[,3]), 
+                                           proj4string = CRS(CRSstring_GM))
+  
+  pointGrid_sp_UTM <- spTransform(pointGrid_df_z, CRS(CRSstring))
+  
+  raster_z <- raster()
+  extent(raster_z) <- extent(pointGrid_sp_UTM)
+  res(raster_z) <- rep(gridResolution_GM * 1.1, 2)
+  raster_z <- raster::rasterize(pointGrid_sp_UTM, raster_z, field = "Z")
+  raster_z_interp <- raster::disaggregate(raster_z, 
+                                          fact = res(raster_z) / rasterRes_final, 
+                                          method = 'bilinear')
+  
+  if (any(is.na(raster_z_interp[]))) {
+    stop("Missing GM heights: coarsen raster resolution [rasterRes_final]")
+  } 
+  
+  return(raster_z_interp)
+}
+
+getASLraster <- function(latStart, 
+                         lonStart,
+                         grid_extent_padding, 
+                         grid_resolution,
+                         CRSstring, 
+                         CRSstring_GM = "+init=epsg:4326") {
+  
+  SPpointsGM <- makeSPointsForGMheights(latStart, 
+                                        lonStart,
+                                        grid_extent_padding, 
+                                        grid_resolution,
+                                        CRSstring, 
+                                        CRSstring_GM)
+  
+  raster_z_interp <- GM_getAslRaster(SPpointsGM, 
+                                     CRSstring = params$projection)
+  
+  return(raster_z_interp)
+  
+}
